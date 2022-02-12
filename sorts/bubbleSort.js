abe43c5856a5223ca2aa56699622324ad08285cf
OsirisAnubis <olegshulgakalendar@gmail.com>
2022-02-12 12:58:48 +0300

    Fix style. Change tab to 2 space and change j+1 to j + 1

--- sorts/bubbleSort.js
+++ sorts/bubbleSort.js
@@ -1,13 +1,13 @@
 const bubbleSort = (list) => {
-	for(let i = 0; i < list.length - 1; i++) {
-		for(let j = 0; j < list.length - 1 - i; j++){
-			if(list[j] > list[j+1]) {
-				let t = list[j];
-				list[j] = list[j+1];
-				list[j+1] = t;
-			}
-		}
-	}
+  for(let i = 0; i < list.length - 1; i++) {
+    for(let j = 0; j < list.length - 1 - i; j++){
+      if(list[j] > list[j + 1]) {
+        let t = list[j];
+        list[j] = list[j + 1];
+        list[j + 1] = t;
+      }
+    }
+  }
 
-	return list;
+  return list;
 };
